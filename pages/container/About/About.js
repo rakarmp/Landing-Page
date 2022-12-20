eda96967b491c03c55ae6602915dda6ef2a10baa
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-12-20 23:22:55 +0700

    change using const to import images

--- pages/container/About/About.js
+++ pages/container/About/About.js
@@ -18,8 +18,8 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">
-        I Know that <span>Good Design</span> <br />
-        means <span>Good Business</span>
+        Maniac <span>Learning</span> <br />
+        Lets <span>Learning</span>
       </h2>
 
       <div className="app__profiles">
--- pages/container/Header/Header.js
+++ pages/container/Header/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 import { motion } from "framer-motion";
 
 import { AppWrap } from "../../wrapper";
-import { images } from "../../constants/index";
 
 const scaleVariants = {
   whileInView: {
@@ -15,9 +14,11 @@ const scaleVariants = {
   },
 };
 
-const flutter = "assets/flutter.png";
+const nextjs = "nextjs.png";
 const redux = "assets/redux.png";
 const sass = "assets/sass.png";
+const gua = "gua.png";
+const circle = "assets/circle.svg";
 
 const Header = () => (
   <div className="app__header app__flex">
@@ -47,11 +48,11 @@ const Header = () => (
       transition={{ duration: 0.5, delayChildren: 0.5 }}
       className="app__header-img"
     >
-      <img src={images.profile} alt="profile_bg" />
+      <img src={gua} alt="profile_bg" />
       <motion.img
         whileInView={{ scale: [0, 1] }}
         transition={{ duration: 1, ease: "easeInOut" }}
-        src={images.circle}
+        src={circle}
         alt="profile_circle"
         className="overlay_circle"
       />
@@ -62,7 +63,7 @@ const Header = () => (
       whileInView={scaleVariants.whileInView}
       className="app__header-circles"
     >
-      {[flutter, redux, sass].map((circle, index) => (
+      {[nextjs, redux, sass].map((circle, index) => (
         <div className="circle-cmp app__flex" key={`circle-${index}`}>
           <img src={circle} alt="profile_bg" />
         </div>
