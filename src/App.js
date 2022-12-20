1898126ee1fd9d143529140837341c3f87688a20
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-12-20 10:58:56 +0700

    alhamdullilah

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div></div>;
-}
+import { Navbar } from "./components";
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import "./App.scss";
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactDOM from "react-dom";
 
+import App from "./App";
 import "./index.css";
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
