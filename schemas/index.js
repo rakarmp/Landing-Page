bfd530cc903726dfb891f82ed4d4eb631eb6d66e
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-12-20 10:07:52 +0700

    fixing schemas

--- schemas/index.js
+++ schemas/index.js
@@ -1,3 +1,19 @@
+import abouts from './abouts'
+import brands from './brands'
 import testimonials from './testimonials'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
 
-export const schemaTypes = [testimonials]
+export const schemaTypes = [
+  testimonials,
+  abouts,
+  brands,
+  contact,
+  experiences,
+  skills,
+  workExperience,
+  works,
+]
